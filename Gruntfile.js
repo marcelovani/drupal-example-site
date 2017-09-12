a8bafb81de8a3211ce529e163572c61d41912b42
Christopher Kinch <Christopher_Kinch@dennis.co.uk>
2017-09-12 16:54:46 +0100

    Adding example package.json, grunt and Gruntfile

---
+++ Gruntfile.js
@@ -0,0 +1,26 @@
+/*global module:false*/
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    // Metadata.
+    pkg: grunt.file.readJSON('package.json'),
+    banner: '/*! <%= pkg.title || pkg.name %> - v<%= pkg.version %> - ' +
+      '<%= grunt.template.today("yyyy-mm-dd") %>\n' +
+      '<%= pkg.homepage ? "* " + pkg.homepage + "\\n" : "" %>' +
+      '* Copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg.author.name %>;' +
+      ' Licensed <%= _.pluck(pkg.licenses, "type").join(", ") %> */\n',
+    meta: {
+      distroPath: '../../profiles/dennis_distro'
+    },
+    // Task configuration.
+
+  });
+
+  // Load plugins to provide necessary tasks.
+  // grunt.loadNpmTasks('module-name-here');
+
+  // Default task.
+  grunt.registerTask('default', []);
+
+};
